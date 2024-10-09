05a261e67dbf453aa4d59382e79268f474a6aa5b
PoojaSharmasuper <dhakadsonu196@gmail.com>
2024-10-09 22:30:52 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -78,9 +78,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         poster_url = await get_imdb(movie_name)
         caption_message = f"#New_File_Added ✅\n\nFile_Name:- <code>{movie_name}</code>\n\nLanguage:- {language}\n\nQuality:- {quality}"    
         movie_update_channel = await db.movies_update_channel_id()    
-        btn = [
-            [InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
-        ]
+        btn = [[InlineKeyboardButton('Get File', url=f'https://telegram.me/{temp.U_NAME}?start=getfile-{filenames}')]]
         reply_markup = InlineKeyboardMarkup(btn)
         if poster_url:
             await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
